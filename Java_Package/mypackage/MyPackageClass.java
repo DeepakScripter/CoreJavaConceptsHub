5d9fa3bbc5b4b21ca84db49f3a3649dede398698
Deepak Vishwakarma <coder.deepak44@gmail.com>
2024-03-13 01:02:07 +0530

    📦 Add MyPackageClass.java to mypackage  This commit adds MyPackageClass.java to the mypackage package. MyPackageClass.java contains a public method `publicMethod()` and a protected method `protectedMethod()`.  1. `publicMethod()` is a public method that prints a message to the console. 2. `protectedMethod()` is a protected method that prints a message to the console.  These methods demonstrate the usage of access modifiers in Java to control the visibility and accessibility of methods within a class. 🌟

---
+++ Java_Package/mypackage/MyPackageClass.java
@@ -0,0 +1,12 @@
+// File: mypackage/MyPackageClass.java
+package mypackage;
+
+public class MyPackageClass {
+    public void publicMethod() {
+        System.out.println("Public method in MyPackageClass");
+    }
+
+    protected void protectedMethod() {
+        System.out.println("Protected method in MyPackageClass");
+    }
+}
