cccdbc9b9fdddf2d1fc934682d6657e7dbbb6ee2
Deepak Vishwakarma <coder.deepak44@gmail.com>
2024-03-13 00:17:41 +0530

    📅 Added Day enum for representing days of the week.
    🌟 This commit introduces the Day enum, which represents the days of the week (SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY). It includes a main method to demonstrate the usage of the enum by iterating over its values and printing them. Enums are a useful tool for representing a fixed set of constants, in this case, the days of the week, providing clarity and type safety in code.

---
+++ Datetime_Operations/Day.java
@@ -0,0 +1,9 @@
+public enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+
+    public static void main(String[] args) {
+        for (Day day : Day.values()) {
+            System.out.println(day);
+        }
+    }
+}
